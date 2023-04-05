470536d3a31f83d8cc38532e33b3059e91a37657
jake <hoinh@gmail.com>
2023-04-05 22:19:46 +0900

    Revert "주석처리"
    This reverts commit 92d6be0cd816a02b51dc1fa7c3b1d2c640e66a44.

--- src/views/WriteView.vue
+++ src/views/WriteView.vue
@@ -2,26 +2,27 @@
   <div class="about">
     <section>
       <div v-for="(paragraph, index) in paragraph_list" :key="index" >
-        <p v-if="(paragraph.tag === 'p')" :id="'__'+index" contenteditable="true"  @keydown="$event=>readText(index, $event)" class="editor" >{{ paragraph.content }}</p>
-        <h1 v-else-if="(paragraph.tag === 'h1')" :id="'__'+index" contenteditable="true"  @keydown="$event=>readText(index, $event)" class="editor" >{{ paragraph.content }}</h1>
-        <blockquote v-else-if="(paragraph.tag === 'blockquote')" :id="'__'+index" contenteditable="true"  @keydown="$event=>readText(index, $event)" class="editor" >{{ paragraph.content }}</blockquote>
+        <p v-if="(paragraph.tag === 'p')" :id="'__'+index" contenteditable="true"  @keydown="$event=>readText(index, $event)" >{{ paragraph.content }}</p>
+        <h1 v-else-if="(paragraph.tag === 'h1')" :id="'__'+index" contenteditable="true"  @keydown="$event=>readText(index, $event)" >{{ paragraph.content }}</h1>
+        <blockquote v-else-if="(paragraph.tag === 'blockquote')" :id="'__'+index" contenteditable="true"  @keydown="$event=>readText(index, $event)" >{{ paragraph.content }}</blockquote>
       </div>
     </section>
     
-    <!-- <article v-html="viewParagraph.body"></article>
-    <textarea rows="5" input="paragraph" @input="$event=>readParagraph($event)">{{paragraph.body}}</textarea> -->
+    <article v-html="viewParagraph.body"></article>
+    <textarea rows="5" input="paragraph" @input="$event=>readParagraph($event)">{{paragraph.body}}</textarea>
     <button type="button" @click="saveParagraph">SAVE</button>
   </div>
 </template>
 
 <script setup>
 import { reactive, onMounted , nextTick} from 'vue'
+import { RouterLink,  useLink } from 'vue-router'
 import { marked } from 'marked';
 
 const paragraph_list = reactive([
   {
     tag : 'p',
-    content:''
+    content:'kevin'
   },
 ])
 
@@ -49,6 +50,32 @@ const readText = async (index, event)=>
     }
     
   }
+  if(event.keyCode === 8)//return p tag by pressing delete key at first position 
+  {
+    if(window.getSelection().anchorOffset === 0)
+    {
+      paragraph_list[index].tag = 'p';
+      paragraph_list[index].content = event.target.innerText;
+
+      await nextTick();
+      document.querySelector(`#__${index}`).focus();
+    }
+  }
+  else //normal
+  {
+    const current_tag_name = event.target.tagName.toLowerCase();
+    const tag_name = readFirstWord(event.target.innerText.substring(0,10), current_tag_name);
+
+    if(tag_name !== current_tag_name)
+    {
+      paragraph_list[index].tag = tag_name;
+      paragraph_list[index].content = event.target.innerText.substring(1);
+      await nextTick();
+      document.querySelector(`#__${index}`).focus();
+    }
+    
+    
+  }
 }
 
 //문장의 첫  단어가 마크테그인지 확인
@@ -57,7 +84,7 @@ const readFirstWord = (str) =>
   const first_word = str.substring(0,1);
   if(first_word === '#') return 'h1';
   else if(first_word === '>') return 'blockquote';
-  else return 'p';
+  else return current_tag_name;
   // const regex = /^[^\s]+(\s[^#]+)?/;
   // const match = str.match(regex);
   // const result = (match)?  match[0] : 'Ops';
@@ -74,76 +101,83 @@ const changeElementTag = (target, tagName) =>
 
 }
 
-// const paragraph = reactive({
-//   id: 0,
-//   body:'',
-//   folder:{},
-//   page : 1,
-//   focus : true,
-// });
 
-
-// const paragraphEditors = reactive([]);
-// paragraphEditors.push(paragraph)
-
-computed: {
-  // paragraphEditors: function () {
-  //   return _.orderBy(this.users, 'name')
-  // }
-}
-
-onMounted(() => 
-{
-  //마지막 엘리먼트에 포커스를 둔다.
-  // const editors = document.querySelectorAll('.editor');
-  // editors[editors.length-1].focus();
-
+const paragraph = reactive({
+  id: 0,
+  body:'',
+  folder:{},
+  page : 1,
+  focus : true,
 });
-  
-
-  // marked.setOptions({breaks:true,gfm:true})
-  // const paragraphId = useLink({...RouterLink.props}).route.value.params.id;
-  // if(paragraphId) getParagraphAtLocalStorage(paragraphId);
 
 
+const paragraphEditors = reactive([]);
+paragraphEditors.push(paragraph)
 
-// const viewParagraph = reactive({
-//   body:'',
-// })
-
-// const readParagraph = (event)=>{
-  
-//   viewParagraph.body = marked.parse(event.target.value);
-//   paragraph.body = event.target.value
-// }
-
-// const readContent = (index, event)=>
-// {
-//   if(event.keyCode === 13)
-//   {
-//     event.preventDefault();
-//     addNewEditor(index);
+// computed: {
+//   paragraphEditors: function () {
+//     return _.orderBy(this.users, 'name')
 //   }
 // }
 
-// const addNewEditor = async (index) => 
-// {
-//   const paragraph_temp = reactive({
-//       id:index+1,
-//       body:'',
-//       folder:{},
-//       page : 1,
-//   });
-//   paragraphEditors.splice(index+1, 0, paragraph_temp);
-//   console.log(paragraphEditors)
-//   await nextTick();
-//   document.querySelector(`p#_${index+1}`).focus();
-// }
+onMounted(() => 
+{
+  marked.setOptions({breaks:true,gfm:true})
+  const paragraphId = useLink({...RouterLink.props}).route.value.params.id;
+  if(paragraphId) getParagraphAtLocalStorage(paragraphId);
 
+})
 
-// const setViewParagraph = (text)=>{
-//   viewParagraph.body = marked.parse(text);
-// }
+const viewParagraph = reactive({
+  body:'',
+})
+
+const readParagraph = (event)=>{
+  
+  viewParagraph.body = marked.parse(event.target.value);
+  paragraph.body = event.target.value
+}
+
+const readContent = (index, event)=>
+{
+  if(event.keyCode === 13)
+  {
+    event.preventDefault();
+    addNewEditor(index);
+  }
+}
+
+const addNewEditor = async (index) => 
+{
+  const paragraph_temp = reactive({
+      id:index+1,
+      body:'',
+      folder:{},
+      page : 1,
+  });
+  paragraphEditors.splice(index+1, 0, paragraph_temp);
+  console.log(paragraphEditors)
+  await nextTick();
+  document.querySelector(`p#_${index+1}`).focus();
+}
+
+const createNewEditor = (node) =>
+{
+  // const p_editor_element = document.createElement('p');
+  // p_editor_element.classList.add('editor');
+  // p_editor_element.setAttribute('contenteditable', true);
+
+  //const p_editor_element = 
+  //console.log(p_editor_element)
+  // node.after(p_editor_element);
+  // console.log(node.nextSibling.focus())
+  // const new_editor_element = node.cloneNode(true);
+  //node.after(Editor);
+}
+
+const setViewParagraph = (text)=>{
+  viewParagraph.body = marked.parse(text);
+}
 
 
 const saveParagraph = ()=>
@@ -157,22 +191,22 @@ const saveParagraph = ()=>
   localStorage.setItem(storageParagraph.id, JSON.stringify(storageParagraph))
 }
 
-// const getParagraphAtLocalStorage = (id) =>
-// {
-//   const localStorageData = localStorage.getItem(id);
-//   try
-//   {
-//     const localPargaraph = (localStorageData)? localStorageData: null;
-//     if(!localPargaraph) return;
-//     Object.assign(paragraph, JSON.parse(localPargaraph));
-//     setViewParagraph(paragraph.body);
-//   }
-//   catch(e)
-//   {
-//     // 
-//   }
+const getParagraphAtLocalStorage = (id) =>
+{
+  const localStorageData = localStorage.getItem(id);
+  try
+  {
+    const localPargaraph = (localStorageData)? localStorageData: null;
+    if(!localPargaraph) return;
+    Object.assign(paragraph, JSON.parse(localPargaraph));
+    setViewParagraph(paragraph.body);
+  }
+  catch(e)
+  {
+    // 
+  }
   
-// }
+}
 
 const util = {
   getNow : () =>
@@ -213,12 +247,32 @@ article{
         width: 100%;
         position: relative;
 }
+/* article::before{
+    content: "";
+    background: url('../assets/image/floral-2069810_640.png');
+    background-size: cover;
+    opacity: 0.5;
+    position: absolute;
+    top: 0px;
+    left: 0px;
+    right: 0px;
+    bottom: 0px;
+
+    -moz-filter: url("data:image/svg+xml;utf8,<svg xmlns=\'http://www.w3.org/2000/svg\'><filter id=\'grayscale\'><feColorMatrix type=\'matrix\' values=\'0.3333 0.3333 0.3333 0 0 0.3333 0.3333 0.3333 0 0 0.3333 0.3333 0.3333 0 0 0 0 0 1 0\'/></filter></svg>#grayscale");
+    -o-filter: url("data:image/svg+xml;utf8,<svg xmlns=\'http://www.w3.org/2000/svg\'><filter id=\'grayscale\'><feColorMatrix type=\'matrix\' values=\'0.3333 0.3333 0.3333 0 0 0.3333 0.3333 0.3333 0 0 0.3333 0.3333 0.3333 0 0 0 0 0 1 0\'/></filter></svg>#grayscale");
+    -webkit-filter: grayscale(100%);
+    filter: gray;
+    filter: url("data:image/svg+xml;utf8,<svg xmlns=\'http://www.w3.org/2000/svg\'><filter id=\'grayscale\'><feColorMatrix type=\'matrix\' values=\'0.3333 0.3333 0.3333 0 0 0.3333 0.3333 0.3333 0 0 0.3333 0.3333 0.3333 0 0 0 0 0 1 0\'/></filter></svg>#grayscale");
+}
+article *{
+  position: relative;
+} */
 
 
 p.editor{width:100%; border: 1px solid #eee;}
 textarea{width:100%;}
 h1{margin-bottom:12px;}
-
+p{margin-bottom:12px;}
 pre{padding: 16px;
         overflow: auto;
         font-size: 85%;
